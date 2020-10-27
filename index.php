c725ada7717663c0f647a4c1c34cf1e91513461f
hetref <70653460+hetref@users.noreply.github.com>
2020-10-27 09:49:03 +0530

    helko

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (empty($name) || empty($email) || empty($message)) {
       echo "Please fill all the fields";
 } else {
       mail("shindearyan179@gmail.com", "Web message", $message, "From: $name <$email>");
-      echo "<script type='text/javascript'>alert'Your message sent succesfully');
+      echo "<script type='text/javascript'>alert('Your message sent succesfully');
 	    window.history.log(-1);
 	   </script>";
 }
