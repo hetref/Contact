d5b61dce2c2ff84a1b4cdb47350f31baa5e18344
hetref <70653460+hetref@users.noreply.github.com>
2020-10-27 09:31:20 +0530

    first

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//variable setting
+$name = $_REQUEST['Name'];
+$email = $_REQUEST['Email'];
+$message = $_REQUEST['Message'];
+
+//check input fields
+if (empty($name) || empty($email) || empty($message)) {
+      echo "Please fill all the fields";
+} else {
+      mail("shindearyan179@gmail.com", "Web message", $message, "From: $name <$email>");
+      echo "<script type='text/javascript'>alert'Your message sent succesfully');
+	    window.history.log(-1);
+	   </script>";
+}
